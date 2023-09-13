97bf89dc8fafc2feaa8ef1e3b2d39adf1d413ab1
femellone <38559436+femellone@users.noreply.github.com>
2023-09-13 03:38:05 -0400

    replace the tailwind colors with custom ones.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,6 @@
+// import the default theme
+// import colors from "tailwindcss/colors"; // In case you want to add the default colors uncoment this import and use them in the theme section
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,7 +12,11 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#3b0d91",
+      secondary: "#0077c2",
+      black: "#2b2d42",
+    },
   },
   plugins: [],
 };
