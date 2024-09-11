cd4dd1f6dde1d2d7b663d6e61036c07d50fa032d
Fernando Mellone <fernandomellone2@gmail.com>
2024-09-11 16:38:50 -0400

    upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/color-mode", "nuxt-icon"],
+
   colorMode: {
     classSuffix: "",
   },
+
   routeRules: {
     "/": { ssr: false },
   },
+
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: "2024-09-11",
 });
